82b327884c3267575f6f914a92eb9098b9bbe536
benthibaud <b_thibaud@laposte.net>
2024-03-28 16:48:28 +0100

    Moineau friquet, fontes woff, script pour mise en cache,..

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 
 [Site de l'association](https://inicitoy.toile-libre.org)
 
-![Alt text](./assets/img/git-screenshot.webp?raw=true "Copie d'écran de la page d'accueil (sept 2023)")
+![Alt text](./assets/img/git-screenshot.webp?raw=true "Copie d'écran de la page d'accueil (mars 2024)")
---
+++ service-worker.js
@@ -0,0 +1,30 @@
+// service-worker.js
+const cacheName = 'inicitoy-cache-v1';
+const filesToCache = [
+  'about.html',
+  '_QRcode.html',
+  'assets/fonts/',
+  'assets/img/',
+  'assets/styles/'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(function(cache) {
+        return cache.addAll(filesToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
